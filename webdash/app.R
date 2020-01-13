0a9f9277d89ea4555f5185049725107abcf0b5f9
adamstruck <adamjstruck@gmail.com>
2020-01-13 11:13:19 -0800

    webdash: model view

--- webdash/app.R
+++ webdash/app.R
@@ -52,11 +52,19 @@ ui <- dashboardPage(
                     box(
                         width = 12,
                         br(),
-                        withSpinner(iheatmaprOutput("predHeatmap", height = "800px")),
-                        br()
+                        withSpinner(iheatmaprOutput("predHeatmap", height = "100%")),
+                        tags$div(align = "center",
+                                 class = "multicol",
+                        shinyWidgets::prettyCheckboxGroup("predHeatmap_axes",
+                                                          label = "Show Labels:",
+                                                          choices = c("Models" = "models",
+                                                                      "Samples" = "samples"),
+                                                          inline = TRUE,
+                                                          bigger = TRUE,
+                                                          width = "100%"))
                     )
                 ),
-                h1("Sample Predictions Accross Repeat Folds"),
+                h1("Subtype Prediction Stability Accross Repeat Folds"),
                 # Sample Selection
                 fluidRow(
                     box(
@@ -68,7 +76,8 @@ ui <- dashboardPage(
                 fluidRow(
                     box(
                         width = 12,
-                        withSpinner(plotlyOutput('samplePredDetails'))
+                        height = "100%",
+                        withSpinner(plotlyOutput("samplePredDetails", height = "100%", inline=T))
                     )
                 )
             ),
@@ -82,6 +91,16 @@ ui <- dashboardPage(
 
 server <- function(input, output) {
     # preds_df <- getPredictions(input$cancer_selection)
+    avg_pred <- preds_df %>%
+        dplyr::group_by(model_id, sample_id) %>%
+        dplyr::count(predicted_value) %>%
+        dplyr::mutate(freq = n / sum(n)) %>%
+        dplyr::arrange(desc(n)) %>%
+        dplyr::slice(1) %>%
+        dplyr::ungroup() %>%
+        dplyr::mutate(
+            predicted_value = as.numeric(predicted_value)
+        )
     nsubtypes <- length(levels(preds_df$actual_value))
     colors <- viridis::viridis(nsubtypes)
     col_map <- as.list(colors)
@@ -94,24 +113,33 @@ server <- function(input, output) {
                        multiple = T)
     })
 
+    # output$modelStability <- renderPlotly({
+    #     g <- avg_pred %>%
+    #         ggplot(aes(x = reorder(model_id, freq, FUN = median), y = freq)) +
+    #         geom_boxplot() +
+    #         labs(y = "", x = "", fill = "") +
+    #         coord_flip() +
+    #         theme_minimal()
+    #     ggplotly(g)
+    # })
+
     output$samplePredDetails <- renderPlotly({
         if (length(input$sample_selection) > 0) {
             df_subset <- preds_df %>%
                 filter(sample_id %in% input$sample_selection)
             g <- ggplot(df_subset, aes(x = model_id)) +
-                geom_bar(aes(fill = predicted_value)) +
+                geom_bar(aes(fill = predicted_value), position = "fill") +
                 scale_fill_manual(values=c(as.character(col_map), "grey")) +
-                labs(y = "Subtype Prediction by Repeat Fold", x = "", fill = "") +
-                scale_y_continuous(expand = c(0, 0.1)) +
+                labs(y = "", x = "", fill = "") +
+                scale_y_continuous(expand = expand_scale(add = c(0.01, 0.05))) +
                 coord_flip() +
-                theme_minimal() +
-                theme(axis.text.x = element_blank(),
-                      axis.ticks.x = element_blank(),
-                      axis.ticks.y = element_blank()) +
-                facet_grid(. ~ sample_id)
-            ggplotly(g)
+                theme_minimal(10) +
+                theme(axis.ticks.y = element_blank(),
+                      panel.grid.major = element_blank()) +
+                facet_wrap(~sample_id, ncol = 2)
+            ggplotly(g) %>% layout(height = 15*length(unique(df_subset$model_id))*ceiling(length(input$sample_selection)/2))
         } else {
-            g <- ggplot(data.frame(x = 1, y = 1, z = "Select one or more Samples"),
+            g <- ggplot(data.frame(x = 1, y = 1, z = "Select One or More Samples"),
                    aes(x, y)) +
                 geom_text(aes(label = z), size = 10) +
                 theme_void()
@@ -120,16 +148,6 @@ server <- function(input, output) {
     })
 
     output$predHeatmap <- renderIheatmap({
-        avg_pred <- preds_df %>%
-            dplyr::group_by(model_id, sample_id) %>%
-            dplyr::count(predicted_value) %>%
-            dplyr::mutate(freq = n / sum(n)) %>%
-            dplyr::arrange(desc(n)) %>%
-            dplyr::slice(1) %>%
-            dplyr::ungroup() %>%
-            dplyr::mutate(
-                predicted_value = as.numeric(predicted_value)
-                )
 
         avg_pred_wide <- avg_pred %>%
             dplyr::select(-n, -freq) %>%
@@ -137,12 +155,6 @@ server <- function(input, output) {
 
         pred_mat <- avg_pred_wide %>% dplyr::select(-model_id) %>% as.matrix()
         row.names(pred_mat) <- avg_pred_wide$model_id
-
-        # freq_pred_wide <- avg_pred %>%
-        #     dplyr::select(-n, -predicted_value) %>%
-        #     tidyr::spread(sample_id, freq)
-        # freq_mat <- freq_pred_wide %>% dplyr::select(-model_id) %>% as.matrix()
-        # row.names(freq_mat) <- freq_pred_wide$model_id
 
         # subtype annotation
         subtypes <-  preds_df %>%
@@ -170,9 +182,9 @@ server <- function(input, output) {
         colClust <- order.dendrogram(as.dendrogram(hclustfunc(t(pred_mat))))
         rowClust <- order.dendrogram(as.dendrogram(hclustfunc(pred_mat)))
 
-        main_heatmap(pred_mat,
+        hm <- main_heatmap(pred_mat,
                      colors = colors,
-                     show_colorbar = T,
+                     show_colorbar = FALSE,
                      col_order = colClust,
                      row_order = rowClust) %>%
             add_col_groups(subtypes,
@@ -180,10 +192,21 @@ server <- function(input, output) {
                            title = "Actual Subtype",
                            side = "top",
                            colors = colors) %>%
-            add_col_title("Samples", font = list(size = 20)) %>%
-            add_row_title("Models", font = list(size = 20)) %>%
             add_col_plot(y =  tpr_sample, layout = list(title = "TPR")) %>%
             add_row_plot(x =  tpr_model, layout = list(title = "TPR"))
+
+        if ("samples" %in% input$predHeatmap_axes) {
+            hm <- hm %>% add_col_labels()
+        } else {
+            hm <- hm %>% add_col_title("Samples", font = list(size = 20))
+        }
+
+        if ("models" %in% input$predHeatmap_axes) {
+            hm <- hm %>% add_row_labels()
+        } else {
+            hm <- hm %>% add_row_title("Models", font = list(size = 20))
+        }
+        hm %>% modify_layout(list(height = 15*dim(pred_mat)[1]))
     })
 }
 
